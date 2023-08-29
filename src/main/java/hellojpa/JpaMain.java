019ea142ae31155d9377fdcb493e17de7ae8dd52
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-29 17:44:42 +0900

    JPA 기본편 개발환경 구축 완료

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try{
+      /*Member member = new Member();
+      member.setId(2L);
+      member.setName("HelloB");
+      em.persist(member);*/
+
+      /*Member findMember = em.find(Member.class, 1L);
+      findMember.setName("HelloJPA");
+
+      em.persist(findMember);   -> 생략 가능*/
+
+      List<Member> result = em.createQuery("select m from Member as m", Member.class)
+              .setFirstResult(5).setMaxResults(8).getResultList();    //페이징이 엄청 간편함
+
+      tx.commit();
+    } catch (Exception e){
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+    emf.close();
+  }
+}
