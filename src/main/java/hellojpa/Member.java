d290a91e6cb6e195192518cb173625c304e5f314
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-09-04 11:33:31 +0900

    매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,16 +3,36 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "MBR")
 @Getter @Setter
 public class Member {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+                                            //Identity
+                                            //sequence
+                                            //table
   private Long id;
-  private String name;
+  @Column(name = "name")
+  private String username;
+
+  private Integer age;
+
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createDate;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+
+  @Lob
+  private String description;
 
   public Member(){
 
