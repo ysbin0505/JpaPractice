6a87d61c7899f2d02059934b80329f660db51ced
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-09-06 14:45:20 +0900

    연관관계 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,4 +1,25 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
 public class Item {
+
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "Item_ID")
+  private Long id;
+
+  @OneToMany(mappedBy = "item")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+  private String name;
+  private int price;
+  private int stockQuantity;
+
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,17 +5,25 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")     //DB에서 ORDER BY 정렬 때문에 인식이 안되는 DB가 종종 발생해서 ORDERS로 명명
 @Getter @Setter
 public class Order {
   @Id @GeneratedValue(strategy = GenerationType.AUTO)
-
+  @Column(name = "ORDER_ID")
   private Long id;
 
-  private Long memberId;
+  @ManyToOne
+  @Column(name = "MEMBER_ID")
+  private Member member;
+
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
   private LocalDateTime orderDate;
   @Enumerated(EnumType.STRING)      //ENUM타입은 이거 써주자 + ORDINARY 안씀 -> 순서꼬임
   private OrderStatus status;
