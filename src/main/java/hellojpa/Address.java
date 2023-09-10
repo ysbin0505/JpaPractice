27b45ee439a7a959967a5a3da3900571b8b67105
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-09-10 16:09:50 +0900

    값 타입 비교 객체 타입은 자바에서 무조건 false 동등성(재정의) 해수 equals 비교하기

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -6,13 +6,31 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 public class Address {
   @Id @GeneratedValue
-  private int id;
+  private Long id;
 
   private String address;
+
+  public Address() {
+    
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Address address1 = (Address) o;
+    return Objects.equals(id, address1.id) && Objects.equals(address, address1.address);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, address);
+  }
 }
--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -14,6 +14,7 @@ public class ValueMain {
 
     System.out.println(" a == b : " + (a == b));
     System.out.println("address1 == address2 " + (address1 == address2));
+    System.out.println("address1 == address2 " + (address1.equals(address2)));
 
   }
 }
