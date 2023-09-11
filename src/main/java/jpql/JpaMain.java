5942a1958d43debe5552882ee0ccb15fa8ccbf10
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-09-11 13:06:37 +0900

    jpql 결과조회 Api

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,17 +14,26 @@ public class JpaMain {
 
     try {
       Member member = new Member();
+      Member member2 = new Member();
       member.setUsername("member1");
       member.setAge(10);
+      member2.setUsername("member2");
+      member2.setAge(5);
       em.persist(member);
+      em.persist(member2);    //persist를 해야 DB반영
 
-      TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+      em.flush();
+      em.clear();
+
+      /*TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
       //TypedQuery<String> stringTypedQuery = em.createQuery("select m.username from Member m", String.class);
       //Query query2 = em.createQuery("select m.username, m.age from Member m", Member.class); //반환 타입이 명확X
       List<Member> resultList = query.getResultList();
       for (Member member1 : resultList) {
-        System.out.println("member1 = " + member1);
-      }
+        System.out.println("member1 = " + member1);*/
+      List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+      Member findMember = result.get(1);    //INdex1번 값을 고치는 의미
+      findMember.setAge(20);
 
       //Member result = query.getSingleResult();    //-> 반환할것이 하나일때
       //System.out.println("result = " + result);
