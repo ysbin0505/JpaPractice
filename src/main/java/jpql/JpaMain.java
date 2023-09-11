a4dd08b81d29a2475979266bcb9cedc7b7812dcb
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-09-11 14:23:32 +0900

    jpql 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,45 +13,29 @@ public class JpaMain {
     tx.begin();
 
     try {
+
+      Team team = new Team();
+      team.setName("teamA");
+      em.persist(team);
+
       Member member = new Member();
       Member member2 = new Member();
       member.setUsername("member1");
       member.setAge(10);
       member2.setUsername("member2");
       member2.setAge(5);
+
+      member.setTeam(team);
+
+
       em.persist(member);
       em.persist(member2);    //persist를 해야 DB반영
 
       em.flush();
       em.clear();
 
-      /*TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-      //TypedQuery<String> stringTypedQuery = em.createQuery("select m.username from Member m", String.class);
-      //Query query2 = em.createQuery("select m.username, m.age from Member m", Member.class); //반환 타입이 명확X
-      List<Member> resultList = query.getResultList();
-      for (Member member1 : resultList) {
-        System.out.println("member1 = " + member1);*/
-      /*List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
-      Member findMember = result.get(1);    //INdex1번 값을 고치는 의미
-      findMember.setAge(20);*/
-
-      //Member result = query.getSingleResult();    //-> 반환할것이 하나일때
-      //System.out.println("result = " + result);
-
-/*      List resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
-      Object o = resultList.get(0);
-      Object[] result = (Object[]) o;
-      System.out.println("result = " + result[0]);
-      System.out.println("result = " + result[1]);
-*/    //Object[]타입으로 여러 값 조회하는 방법
-
-      List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
-      MemberDTO memberDTO = resultList.get(0);
-      System.out.println("memberDTO = " + memberDTO.getUsername());
-      System.out.println("memberDTO = " + memberDTO.getAge());
-
-
-      em.flush();
+      String query = "select m from Member m, Team t where m.username = t.name";
+      List<Member> result = em.createQuery(query, Member.class).getResultList();
 
       tx.commit();
     } catch (Exception e){
